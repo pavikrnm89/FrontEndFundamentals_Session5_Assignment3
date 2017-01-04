3eeaf01284af2e85c66d080b264f7cc2ccc52d41
pavikrnm89 <pavankarnam89@gmail.com>
2017-01-04 12:53:02 +0530

    Create app.js

---
+++ resources/app.js
@@ -0,0 +1,13 @@
+var employee=
+   {
+     Name: "Pavan Karnam",
+     Age: 27,
+     Salary: 20000,
+	 Address:{
+				City:"Bangalore",
+			    State:"Karnataka",
+				PinCode:560056
+			 }
+   };
+   
+console.log(employee);
